cc47cab613a2ca4ba4ead3da6f02ad18fbd16c12
Yugyeong-Ji <2monica9@naver.com>
2022-03-09 16:55:44 +0900

    소프트웨어공학 2주차 실습

---
+++ week2/Die.java
@@ -0,0 +1,19 @@
+package week2;
+
+import java.util.Random;
+
+public class Die {
+  private int faceValue;
+  public Die(){
+    faceValue = 0;
+  }
+
+  public int getFaceValue(){
+    return faceValue;
+  }
+
+  public void roll(){
+    Random random = new Random();
+    faceValue = random.nextInt(6) + 1;
+  }
+}
